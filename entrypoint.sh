2aa136b31b2b932c9403bab774ec7026600f9cca
D-upe <174077233+D-upe@users.noreply.github.com>
2026-06-06 02:01:09 +0300

    Use PORT env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ superset fab create-admin --username admin --firstname Superset --lastname Admin
 superset db upgrade
 superset init
 
-exec gunicorn --bind 0.0.0.0:8088 --workers 1 --timeout 120 "superset.app:create_app()"
+exec gunicorn --bind 0.0.0.0:${PORT:-8088} --workers 1 --timeout 120 "superset.app:create_app()"
